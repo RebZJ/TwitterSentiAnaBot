b119111ea4f2ce2726a21ee2fccbe85477eb6550
RebZJ <rebontoz@gmail.com>
2020-08-10 14:37:12 +1000

    a smol update

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 Just a little twitter sentiment analysis tool
 
-Written in python using an ML model called Vader
+##Important
+Add a file called "Auth.txt" to hold you twitter credenditials, should look something like this:
+
+Replace X with your twitter creds:
+
+{"consumer_key":"XXXXXXXXX",
+"consumer_secret":"XXXXXXXXXXXXX",
+"access_token":"XXXXXXXXXXXXXXXXXXXXX",
+"access_token_secret":"XXXXXXXXXXXXXXXXXXXXXXXX"}
+
+##Useage
+Just use the function senti_analysis(user, number) and use the @ of the user for the user field, and the number is the tweet you want e.g 0 is the latest tweet, 1 is second latest etc.
+
+Example: print(senti_analysis("@elonmusk", 0))
+Prints Elon's Musks latest tweet
+
+#Score
+The score has 4 fields that are pretty self explanatory:
+*Negative
+*Neutral
+*Positive
+*Compound: A combination of all of the above accounted into one
+
+Written in python processing data using NLTK and sentimentent analysis ML model called Vader
+
+
--- sentiment_processing.py
+++ sentiment_processing.py
@@ -5,38 +5,50 @@ from nltk.corpus import stopwords
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+def senti_analysis(user, number):
+	# read the file in for authentication
+	with open('Auth.txt') as json_file:
+		data = json.load(json_file)
+		consumer_key = data["consumer_key"]
+		consumer_secret = data["consumer_secret"]
+		access_token = data["access_token"]
+		access_token_secret = data["access_token_secret"]
 
-consumer_key = 'fJvo5SeYGf83ZBVMVotJ5BfiC'
-consumer_secret = '64pj8JkLhMRZbqhQRxKuKzzH0ob1qEETMH6Kk0hpSHBVSdp1Nn'
-access_token = '1101670693656125440-8UrXC2geQBmpDCrzQ3iaTqYheJLc2Y'
-access_token_secret = 'XgKeiRPXN0Fg7H6kwlGTfjXvl2zEA2SaAhUsEI4OOQ4fA'
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+	analyser = SentimentIntensityAnalyzer()
 
-analyser = SentimentIntensityAnalyzer()
+	api = tweepy.API(auth)
 
-api = tweepy.API(auth)
+	public_tweets = api.user_timeline(user, count=number+1)
 
-public_tweets = api.user_timeline("@elonmusk",count=2)
+	# this is an array
+	tokenized_sentence = (word_tokenize(public_tweets[number].text))
+	words_to_filter = []
 
-#this is an array
-tokenized_sentence = (word_tokenize(public_tweets[1].text))
-words_to_filter = []
+	stop_words = set(stopwords.words("english"))
 
-stop_words=set(stopwords.words("english"))
 
-def strip_words(text_array, words_to_strip):
-	filtered_sentence = []
-	for w in text_array:
-		if w not in words_to_strip:
-			filtered_sentence.append(w)
-	return filtered_sentence
+	def strip_words(text_array, words_to_strip):
+		filtered_sentence = []
+		for w in text_array:
+			if w not in words_to_strip:
+				filtered_sentence.append(w)
+		return filtered_sentence
 
-new_tokenized_sentence = strip_words(tokenized_sentence, stop_words)
 
-def sentiment_analyser_scores(sentence):
-	score = analyser.polarity_scores(sentence)
-	print("{} {}".format(sentence,str(score)))
+	new_tokenized_sentence = strip_words(tokenized_sentence, stop_words)
 
-sentiment_analyser_scores(public_tweets[1].text)
+
+	def sentiment_analyser_scores(sentence):
+		score = analyser.polarity_scores(sentence)
+		print("{} {}".format(sentence, str(score)))
+
+	return {"score": analyser.polarity_scores(public_tweets[number].text),
+        "tweet": public_tweets[number].text,
+        "user": user}
+
+
+#print(senti_analysis("@elonmusk", 0))
+
